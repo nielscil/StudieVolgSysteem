14cbbe6a5aeec4d2088448cbfd382ae6331edf36
nielscil <niels.bouma@outlook.com>
2015-03-05 10:00:38 +0100

    allusss

---
+++ App_Code/Gebruikers.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using WebMatrix.Data;
+using WebMatrix.WebData;
+
+public static class Constants{
+    //database naam invullen
+    public static string DBName = "Demo";
+    //meer constanten toevoegen
+}
+
+public class Gebruiker
+{
+    public int GebruikerID {get;set;}
+    public string Email {get;set;}
+    public string[] Role {get;set;}
+    public string Naam {get;set;}
+    public string Achternaam {get;set;}
+    public string Woonplaats {get;set;}
+    public string Adres {get;set;}
+    public string TelefoonNr {get;set;}
+    public string Postcode {get;set;}
+
+    public Gebruiker(int userID,string email,string[] role,string name,string surname,string hometown,string adress, string telefoonnr,string postcode)
+    {
+        this.Email = email;
+        this.GebruikerID = userID;
+        this.Role = role;
+        this.Naam = name;
+        this.Achternaam = surname;
+        this.Woonplaats = hometown;
+        this.Adres = adress;
+        this.TelefoonNr = telefoonnr;
+        this.Postcode = postcode;
+    }
+    public Gebruiker(string email,string[] role,string name,string surname,string hometown,string adress, string telefoonnr,string postcode)
+    {
+        this.Email = email;
+        this.Role = role;
+        this.Naam = name;
+        this.Achternaam = surname;
+        this.Woonplaats = hometown;
+        this.Adres = adress;
+        this.TelefoonNr = telefoonnr;
+        this.Postcode = postcode;
+    }
+
+    /// <summary>
+    /// Geeft lijst met alle gebruikers
+    /// </summary>
+    /// <returns>lijst van Gebruiker</returns>
+    public static List<Gebruiker> GetAllUsers()
+    {
+        Database db = Database.Open(Constants.DBName);
+        var rows = db.Query("SELECT * FROM gebruikers;");
+        db.Close();
+        List<Gebruiker> lijst = new List<Gebruiker>();
+        foreach(var r in rows)
+        {
+            Gebruiker g = new Gebruiker(r.GebruikerID, r.Email, Roles.GetRolesForUser(r.Email),r.Naam, r.Achternaam, r.Woonplaats, r.Adres, r.Telefoon_nr, r.Postcode);
+           lijst.Add(g);
+        }
+        return lijst;
+    }
+
+    /// <summary>
+    /// Geeft een gebruiker terug
+    /// </summary>
+    /// <param name="userid">gebruikerid van gebruiker</param>
+    /// <returns>gebruiker</returns>
+    public static Gebruiker GetUser(int userid)
+    {
+        Database db = Database.Open(Constants.DBName);
+        var row = db.QuerySingle("SELECT * FROM gebruikers WHERE gebruikerid=@0 ;",userid);
+        db.Close();
+        if(row == null)
+            throw new Exception("Niet gevonden");
+        return new Gebruiker(row.GebruikerID,row.Email,Roles.GetRolesForUser(row.Email),row.Naam,row.Achternaam,row.Woonplaats,row.Adres,row.Telefoon_nr,row.Postcode);
+    }
+
+    /// <summary>
+    /// Maakt gebruiker aan.
+    /// </summary>
+    /// <param name="email">email</param>
+    /// <param name="roles">rollen</param>
+    /// <param name="wachtwoord">wachtwoord</param>
+    /// <param name="naam">naam</param>
+    /// <param name="achternaam">achternaam</param>
+    /// <param name="woonplaats">woonplaats</param>
+    /// <param name="adres">adres</param>
+    /// <param name="telefoonnr">telefoonnummer</param>
+    /// <param name="postcode">postcode</param>
+    /// <returns>true als het goed gegaan is, false wanneer dit niet het geval is</returns>
+    public static bool AddUser(string email,string[] roles,string wachtwoord,string naam,string achternaam,string woonplaats,string adres, string telefoonnr,string postcode)
+    {
+        Database db = Database.Open(Constants.DBName);
+        db.Execute("INSERT INTO gebruikers (Email,naam,achternaam,woonplaats,adres,telefoon_nr,postcode) Values (@0,@1,@2,@3,@4,@5,@6);",email,naam,achternaam,woonplaats,adres,telefoonnr,postcode);
+        db.Close();
+        try
+        {
+            WebSecurity.CreateAccount(email, wachtwoord, false);
+            Roles.AddUserToRoles(email, roles);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Update een bepaalde gebruiker
+    /// </summary>
+    /// <param name="gebruikerid">gebruikeid</param>
+    /// <param name="roles">rollen</param>
+    /// <param name="naam">naam</param>
+    /// <param name="achternaam">achternaam</param>
+    /// <param name="woonplaats">woonplaats</param>
+    /// <param name="adres">adres</param>
+    /// <param name="telefoonnr">telefoonnummer</param>
+    /// <param name="postcode">postcode</param>
+    /// <returns>true als updaten succesvol is, anders false</returns>
+    public static bool UpdateUser(int gebruikerid,string[] roles,string naam,string achternaam,string woonplaats,string adres, string telefoonnr,string postcode)
+    {
+        Database db = Database.Open(Constants.DBName);
+        try
+        {
+            db.Execute("UPDATE gebruikers SET naam=@0,achternaam=@1,woonplaats=@2,adres=@3,telefoon_nr=@4,postcode=@5 WHERE gebruikersid=@6 );", naam, achternaam, woonplaats, adres, telefoonnr, postcode, gebruikerid);
+            db.Close();
+            string gebruikersnaam = GetUser(gebruikerid).Email;
+            foreach (string s in roles)
+            {
+                if (!Roles.IsUserInRole(gebruikersnaam, s))
+                {
+                    Roles.AddUserToRole(gebruikersnaam, s);
+                }
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// reset wachtwoord
+    /// </summary>
+    /// <param name="gebruikersid">gebruikerid</param>
+    public static void ResetPass(int gebruikersid)
+    {
+        WebSecurity.GeneratePasswordResetToken(GetUser(gebruikersid).Email);
+    }
+
+    /// <summary>
+    /// Verwijderd gebruiker
+    /// </summary>
+    /// <param name="userid">gebruikerid</param>
+    /// <returns>true als het succesvol is, anders false</returns>
+    public static bool DeleteUser(int userid)
+    {
+        Database db = Database.Open(Constants.DBName);
+        db.Execute("DELETE FROM webpages_Membership WHERE userid=@0;",userid);
+        db.Execute("DELETE FROM webpages_usersinroles WHERE userid=@0;",userid);
+        db.Execute("DELETE FROM gebruikers WHERE gebruikerid=@0",userid);
+        var row = db.QuerySingle("SELECT * FROM gebruikers WHERE gebruikerid=@0",userid);
+        db.Close();
+        if(row == null)
+            return true;
+        else
+            return false;
+    }
+}
+
+public class Student : Gebruiker
+{
+    public int SLBerID {get;set;}
+    public string Opmerkingen {get;set;}
+    public string StudentNr {get;set;}
+
+    public Student(int studentID,string email,string[] role,string name,string surname,string hometown,string adress, string telefoonnr,string postcode,int slberID,string opmerking,string studentNr) : base(studentID,email,role,name,surname,hometown,adress,telefoonnr,postcode)
+    {
+        SLBerID = slberID;
+        Opmerkingen = opmerking;
+        StudentNr = studentNr;
+    }
+    public Student(string email,string[] role,string name,string surname,string hometown,string adress, string telefoonnr,string postcode,int slberID,string opmerking,string studentNr) : base(email,role,name,surname,hometown,adress,telefoonnr,postcode)
+    {
+        SLBerID = slberID;
+        Opmerkingen = opmerking;
+        StudentNr = studentNr;
+    }
+
+    /// <summary>
+    /// Verwijderd student
+    /// </summary>
+    /// <param name="userid">gebruiker id</param>
+    /// <returns>true als het succesvol is, anders false</returns>
+    public static bool DeleteUser(int userid)
+    {
+        Database db = Database.Open(Constants.DBName);
+        db.Execute("DELETE FROM webpages_Membership WHERE userid=@0;",userid);
+        db.Execute("DELETE FROM webpages_usersinroles WHERE userid=@0;",userid);
+        db.Execute("DELETE FROM gebruikers WHERE gebruikerid=@0",userid);
+        db.Execute("DELETE FROM student WHERE studentid=@0",userid);
+        var row = db.QuerySingle("SELECT * FROM gebruikers WHERE gebruikerid=@0",userid);
+        db.Close();
+        if(row == null)
+            return true;
+        else
+            return false;
+    }
+
+    /// <summary>
+    /// Maakt gebruiker aan.
+    /// </summary>
+    /// <param name="email">email</param>
+    /// <param name="wachtwoord">wachtwoord</param>
+    /// <param name="roles">rollen</param>
+    /// <param name="naam">naam</param>
+    /// <param name="achternaam">achternaam</param>
+    /// <param name="woonplaats">woonplaats</param>
+    /// <param name="adres">adres</param>
+    /// <param name="telefoonnr">telefoonnummer</param>
+    /// <param name="postcode">postcode</param>
+    /// <param name="slberID">slb id</param>
+    /// <param name="opmerking">opmerking</param>
+    /// <param name="studentNr">student nummer</param>
+    /// <returns>true als het goed gegaan is, false wanneer dit niet het geval is</returns>
+    public static bool AddUser(string email,string wachtwoord,string[] roles,string naam,string achternaam,string woonplaats,string adres, string telefoonnr,string postcode,int slberID,string opmerking,string studentNr)
+    {
+        Database db = Database.Open(Constants.DBName);
+        db.Execute("INSERT INTO gebruikers (Email,naam,achternaam,woonplaats,adres,telefoon_nr,postcode) Values (@0,@1,@2,@3,@4,@5,@6);",email,naam,achternaam,woonplaats,adres,telefoonnr,postcode);
+        int id = db.QueryValue("SELECT gebruikerID FROM gebruikers WHERE email = @0",email);
+        db.Execute("INSERT INTO Student (SLBerID,opmerking,studentnummer,studentID) VALUES (@0,@1,@2,@3)",slberID,opmerking,studentNr,id);
+         db.Close();
+        try{
+            WebSecurity.CreateAccount(email, wachtwoord, false);
+            Roles.AddUserToRoles(email, roles);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Update een bepaalde student
+    /// </summary>
+    /// <param name="gebruikerid">gebruikeid</param>
+    /// <param name="roles">rollen</param>
+    /// <param name="naam">naam</param>
+    /// <param name="achternaam">achternaam</param>
+    /// <param name="woonplaats">woonplaats</param>
+    /// <param name="adres">adres</param>
+    /// <param name="telefoonnr">telefoonnummer</param>
+    /// <param name="postcode">postcode</param>
+    /// <param name="slberID">slber id</param>
+    /// <param name="opmerking">opmerking</param>
+    /// <param name="studentNr">studenten nummer</param>
+    /// <returns>true als updaten succesvol is, anders false</returns>
+    public static bool UpdateUser(int gebruikerid,string[] roles,string naam,string achternaam,string woonplaats,string adres, string telefoonnr,string postcode,int slberID,string opmerking,string studentNr)
+    {
+        Database db = Database.Open(Constants.DBName);
+        try
+        {
+            db.Execute("UPDATE gebruikers SET naam=@0,achternaam=@1,woonplaats=@2,adres=@3,telefoon_nr=@4,postcode=@5 WHERE gebruikerid=@6;", naam, achternaam, woonplaats, adres, telefoonnr, postcode, gebruikerid);
+            db.Execute("UPDATE Student SET SLBerID=@0,opmerking=@1,studentnummer=@2 WHERE studentid=@3;", slberID, opmerking, studentNr, gebruikerid);
+            db.Close();
+            string gebruikersnaam = GetUser(gebruikerid).Email;
+            foreach (string s in roles)
+            {
+                if (!Roles.IsUserInRole(gebruikersnaam, s))
+                {
+                    Roles.AddUserToRole(gebruikersnaam, s);
+                }
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static List<Student> GetStudents()
+    {
+        Database db = Database.Open(Constants.DBName);
+        var rows = db.Query("SELECT * FROM gebruikers,student WHERE gebruikerID = studentID");
+        db.Close();
+        List<Student> lijst = new List<Student>();
+        foreach(var r in rows)
+        {
+
+            Student s = new Student(r.GebruikerID, r.Email, Roles.GetRolesForUser(r.Email), r.Naam, r.Achternaam, r.Woonplaats, r.Adres, r.Telefoon_nr, r.postcode, r.SLBerID, r.Opmerking, r.StudentNummer);
+            lijst.Add(s);
+        }
+        return lijst;
+    }
+
+    public static Student GetStudent(int studentid)
+    {
+         Database db = Database.Open(Constants.DBName);
+        var r = db.QuerySingle("SELECT * FROM gebruikers,student WHERE gebruikerID = studentID AND gebruikerID =@0",studentid);
+        db.Close();
+        if(r == null)
+            throw new Exception("Geen student gevonden");
+        return new Student(r.GebruikerID,r.Email,Roles.GetRolesForUser(r.Email),r.Naam,r.Achternaam,r.Woonplaats,r.Adres,r.Telefoon_nr,r.postcode,r.SLBerID,r.Opmerking,r.StudentNummer);
+    }
+}
+
+public class SLB : Gebruiker
+{
+    public int SLBerID {get;set;}
+
+    public SLB(int SLBerID,string email,string[] roles,string naam,string achternaam,string woonplaats,string adres,string telefoonnr,string postcode) : base(SLBerID,email,roles,naam,achternaam,woonplaats,adres,telefoonnr,postcode)
+    {
+        
+    }
+
+    public SLB(string email,string[] roles,string naam,string achternaam,string woonplaats,string adres,string telefoonnr,string postcode) : base(email,roles,naam,achternaam,woonplaats,adres,telefoonnr,postcode)
+    {
+        
+    }
+
+    public static bool HasStudent(int slbid)
+    {
+        Database db = Database.Open(Constants.DBName);
+        int r = db.QueryValue("SELECT COUNT(studentID) FROM Student WHERE slberid=@0", slbid);
+        if (r > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static List<SLB> GetSLBers()
+    {
+        Database db = Database.Open(Constants.DBName);
+        var rows = db.Query("SELECT * FROM gebruikers WHERE gebruikerID in (SELECT UserID FROM webpages_UsersInRoles WHERE RoleID = 2);");
+        db.Close();
+        List<SLB> lijst = new List<SLB>();
+        foreach(var r in rows)
+        {
+            SLB slb = new SLB(r.GebruikerID,r.Email,Roles.GetRolesForUser(r.Email),r.Naam,r.Achternaam,r.Woonplaats,r.Adres,r.Telefoon_nr,r.Postcode);
+            lijst.Add(slb);
+        }
+        
+        return lijst;
+    }
+    public static SLB GetSLBer(int slbid)
+    {
+        Database db = Database.Open(Constants.DBName);
+        var r = db.QuerySingle("SELECT * FROM gebruikers WHERE gebruikerID in (SELECT userID FROM webpages_UsersInRoles WHERE RoleID = 2) AND gebruikerID =@0;",slbid);
+        db.Close();
+        if(r == null)
+            throw new Exception("Geen SLB'er gevonden");
+        return new SLB(r.GebruikerID,r.Email,Roles.GetRolesForUser(r.Email),r.Naam,r.Achternaam,r.Woonplaats,r.Adres,r.Telefoon_nr,r.Postcode);
+    }
+
+}
---
+++ App_Code/Validator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web; 
+using System.Web.WebPages;
+/// <summary>
+/// Summary description for Validator
+/// </summary>
+public class PasswordValidator : RequestFieldValidatorBase
+{
+    string name;
+    string surname;
+    string email;
+    public PasswordValidator(string name,string surname,string email,string errorMessage = null): base (errorMessage)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+    /// <summary>
+    /// Checkt of wachtwoord geen namen bevat.
+    /// </summary>
+    /// <param name="httpContext">http context</param>
+    /// <param name="value">wachtwoord</param>
+    /// <returns>geeft true terug wanneer het wachwoord geen namen bevat, anders false </returns>
+    protected override bool IsValid(HttpContextBase httpContext, string value)
+    {
+        bool containsname = value.Contains(name);
+        bool containssurname = false;
+        string[]surnamesplit = surname.Split(' ');
+        foreach(string s in surnamesplit)
+        {
+            if(s.Length > 3)
+            {
+                if(value.Contains(s))
+                {
+                    containssurname = true;
+                }
+            }
+        }
+        string emailname = email.Split('@')[0];
+        bool containsemail = value.Contains(emailname);
+        return !containsemail && !containsname && !containssurname;
+    }
+
+}
+public class Regexval : RequestFieldValidatorBase
+{
+    string regex;
+    public Regexval(string regex,string errorMessage = null): base(errorMessage)
+    {
+        this.regex = regex;
+    }
+    /// <summary>
+    /// checkt of de regex matcht met de inkomende value
+    /// </summary>
+    /// <param name="httpContext">http context</param>
+    /// <param name="value">inkomende waarde om te vergelijken</param>
+    /// <returns>true als t matcht, anders false</returns>
+    protected override bool IsValid(HttpContextBase httpContext, string value)
+    {
+        return Regex.IsMatch(value, regex);
+    }
+
+}
+
+public class MyValidator
+{
+    public static IValidator Password(string name,string surname,string email,string errorMessage = null){
+        if(string.IsNullOrEmpty(errorMessage)){
+            errorMessage = "Er ging iets fout";
+        }
+        return new PasswordValidator(name,surname,email,errorMessage);
+    }
+
+    public static IValidator Regex(string regex, string errorMessage = null)
+    {
+        if (string.IsNullOrEmpty(errorMessage))
+        {
+            errorMessage = "Komt niet overeen";
+        }
+        return new Regexval(regex, errorMessage);
+    }
+}
